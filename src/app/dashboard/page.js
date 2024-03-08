e6f06d3f9e1c04bf18cec93c6fecd6729ea664cd
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-08 14:58:26 +0600

    meta data

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Next | Dashboard",
+  description: "Dashboard",
+};
+
 export default function Page() {
   return <h1>Hello, Dashboard Page!</h1>;
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
+import img from "@/assets/abstract-uv-ultraviolet-light-composition.jpg";
 
 export default function Page() {
-  return <Link href="/dashboard">Dashboard</Link>;
+  return (
+    <div>
+      <Link href="/dashboard">Dashboard</Link>
+      {/* <img src={img} height={500} width={500} /> */}
+      <Image className="relative" src={img} fill />
+      <h1 className="absolute text-white">Hello</h1>
+    </div>
+  );
 }
