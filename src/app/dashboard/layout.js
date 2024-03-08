b53170dc96c7a50d2a7bf2218eaaaf052051e56f
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-08 12:39:15 +0600

    imported a bg img

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,5 +1,3 @@
-export default function DashboardLayout({
-  children, // will be a page or nested layout
-}) {
+export default function DashboardLayout({ children }) {
   return <section>{children}</section>;
 }
