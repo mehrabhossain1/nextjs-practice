12bd4e2efbb648f4b92208b2b94fd9dbe0a379ac
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-10 02:02:04 +0600

    relative absolute

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,15 @@ import img from "@/assets/abstract-uv-ultraviolet-light-composition.jpg";
 export default function Page() {
   return (
     <div>
-      <Link href="/dashboard">Dashboard</Link>
+      {/* <Link href="/dashboard">Dashboard</Link> */}
       {/* <img src={img} height={500} width={500} /> */}
-      <Image className="relative" src={img} fill />
-      <h1 className="absolute text-white">Hello</h1>
+      {/* <Image className="relative" src={img} fill /> */}
+      <div className="absolute left-1/4">
+        <h1 className="text-5xl">Hello, I am Mehrab Hossain</h1>
+        <p>I am a Web Developer, I love Next, Tailwind, and React</p>
+        <button>Download CV</button>
+        <button>Hire Me</button>
+      </div>
     </div>
   );
 }
